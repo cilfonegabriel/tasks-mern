4d4dcc6252815b32fa193106a9dd258f4e8a6ffe
gabicilfone <cilfonegabriel@gmail.com>
2023-12-08 09:20:20 -0300

    install dotenv

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,8 +3,7 @@ import mongoose from "mongoose";
 const conectarDB = async () => {
     try {
         const connection = await mongoose.connect("mongodb+srv://root:root@cluster0.cuanb9z.mongodb.net/tasksMern?retryWrites=true&w=majority",{
-           useNewUrlParser: true, 
-           useUnifiedTopology: true,
+
         });  
         
         const url = `${connection.connection.host}:${connection.connection.port}`;
