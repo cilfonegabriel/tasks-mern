c71c65a5f8534fa329c7fd3f0baaff5dd255d68c
gabicilfone <cilfonegabriel@gmail.com>
2023-12-08 09:14:19 -0300

    Add function for connect MongoDb

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,19 @@
 import mongoose from "mongoose";
+
+const conectarDB = async () => {
+    try {
+        const connection = await mongoose.connect("mongodb+srv://root:root@cluster0.cuanb9z.mongodb.net/tasksMern?retryWrites=true&w=majority",{
+           useNewUrlParser: true, 
+           useUnifiedTopology: true,
+        });  
+        
+        const url = `${connection.connection.host}:${connection.connection.port}`;
+
+        console.log(`MongoDB connection on: ${url}`);
+    } catch (error) {
+        console.log(`error: ${error.message}`);
+        process.exit(1);
+    }
+};
+
+export default conectarDB;
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import conectarDB from './config/db.js';
 
 const app = express();
+
+conectarDB();
 
 app.listen(4000, () => {
     console.log('Server runniing on port 4000');
