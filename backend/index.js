27b3a139f632f728ebf2fff1209c98d790b92e9f
gabicilfone <cilfonegabriel@gmail.com>
2023-12-11 12:14:40 -0300

    Add routing for users

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import conectarDB from './config/db.js';
+import userRoutes from './routes/userRoutes.js'
 
 const app = express();
 
 dotenv.config()
 
 conectarDB();
+
+// Routing
+app.use('/api/users', userRoutes)
 
 const PORT = process.env.PORT || 4000;
 
