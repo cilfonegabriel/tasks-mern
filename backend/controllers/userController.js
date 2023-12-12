902a52a1a10612f4a46b10f69aad50aa4645fd8c
gabicilfone <cilfonegabriel@gmail.com>
2023-12-12 17:00:30 -0300

    Check if the password is correct.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,21 +24,29 @@ const register = async (req, res) => {
 const authenticate = async (req, res) => {
     const { email, password } = req.body;
     // Check if the user exists
-    const user = await User.findOne({ email})
+    const user = await User.findOne({ email });
     if(!user) {
         const error = new Error("Username does not exist");
         return res.status(404).json({ msg: error.message });
     }
 
     // Check if the user is confirmed
-    if(!user.confirmed) {
+    if(!user.confirm) {
         const error = new Error("Your account has not been confirmed");
         return res.status(403).json({ msg: error.message });
     }
-    console.log(user)
 
     // Check your password
-
+    if(await user.checkPassword(password)) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        const error = new Error("Password is incorrect");
+        return res.status(403).json({ msg: error.message });
+    }
 }
 
 export {
