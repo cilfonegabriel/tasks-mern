f2219c0c5ef9067ec2f91651276e24f9f07f34c1
gabicilfone <cilfonegabriel@gmail.com>
2023-12-13 16:22:09 -0300

    Storing the new password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -100,7 +100,24 @@ const checkToken = async (req, res) => {
  }
 
  const newPassword = async (req, res) => {
+    const { token } = req.params;
+    const  { password } = req.body;
 
+    const user = await User.findOne({ token });
+
+    if(user) {
+        user.password = password;
+        user.token = "";
+        try {
+            await user.save();
+            res.json({ msg: "Password successfully modified"});
+        } catch (error) {
+            console.log(error);
+        }
+    } else {
+        const error = new Error("Invalid token");
+        return res.status(404).json({ msg: error.message });
+    }
  }
 
 export {
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -9,8 +9,7 @@ router.post('/', register); //Create a new user
 router.post('/login', authenticate);
 router.get('/confirm/:token', confirm);
 router.post("/forgotten-password", forgotPassword);
-router.get("/forgotten-password/:token", checkToken);
-router.post("/forgotten-password/:token", newPassword);
+router.route("/forgotten-password/:token").get(checkToken).post(newPassword);
 
 
 export default router;
