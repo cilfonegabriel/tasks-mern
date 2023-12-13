b51201a2dd8f9005606e97c85023323c9c2639bd
gabicilfone <cilfonegabriel@gmail.com>
2023-12-13 07:20:53 -0300

    Add routing to confirm

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,10 +2,11 @@ import express from 'express';
 
 const router = express.Router();
 
-import { register, authenticate } from '../controllers/userController.js';
+import { register, authenticate, confirm } from '../controllers/userController.js';
 
 // Authentication, confirmation, and user registration.
 router.post('/', register); //Create a new user
 router.post('/login', authenticate);
+router.get('/confirm/:token', confirm)
 
 export default router;
