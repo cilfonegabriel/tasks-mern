88048668be75c25ce18f97b0cc805806d488884c
gabicilfone <cilfonegabriel@gmail.com>
2023-12-13 07:17:31 -0300

    Show id of the user

--- backend/helpers/generateJWT.js
+++ backend/helpers/generateJWT.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
-const generateJWT = () => {
-    return jwt.sign({ name:'Gabriel' }, process.env.JWT_SECRET, {
+const generateJWT = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
         expiresIn: "30d",
     })
 };
