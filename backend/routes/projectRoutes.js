29af70e1705282ab56bb355b24b1d57c8f1ee2ad
gabicilfone <cilfonegabriel@gmail.com>
2023-12-18 15:53:02 -0300

    Import functions

---
+++ backend/routes/projectRoutes.js
@@ -0,0 +1,12 @@
+import { 
+    getProjects,
+    newProject,
+    getProject,
+    editProject,
+    deleteProject,
+    addCollaborator,
+    deleteCollaborator,
+    getTasks,
+ } from "../controllers/projectController";
+
+ 
