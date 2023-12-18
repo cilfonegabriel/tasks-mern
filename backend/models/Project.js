adb14a321c158e949b9d98f64d72e9a214e339bc
gabicilfone <cilfonegabriel@gmail.com>
2023-12-18 15:44:33 -0300

    Creating the model of project

--- backend/models/Project.js
+++ backend/models/Project.js
@@ -1,1 +1,41 @@
 import mongoose from "mongoose";
+
+const projectSchema = mongoose.Schema(
+    {
+        name: {
+            type: 'string',
+            trim: true,
+            required: true,
+        },
+        description: {
+            type: 'string',
+            trim: true,
+            required: true,
+        },
+        deliverDate: {
+            type: 'date',
+            default: Date.now(),
+        },
+        customer: {
+            type: 'string',
+            trim: true,
+            required: true,
+        },
+        creator: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+        },
+        collaborators: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Project = mongoose.model('Project', projectSchema);
+export default Project;
