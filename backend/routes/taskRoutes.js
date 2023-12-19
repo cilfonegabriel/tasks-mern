5484a3443c78c561e992cd2700ad0d51c45e5d8f
gabicilfone <cilfonegabriel@gmail.com>
2023-12-19 10:41:54 -0300

    Correction

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -8,7 +8,7 @@ import {
 } from "../controllers/taskController.js";
 import checkAuth from '../middleware/checkAuth.js';
 
-const router = express.Router
+const router = express.Router();
 
 router.post('/',checkAuth, addTask)
 router.route('/:id').get(checkAuth, getTask).put(checkAuth, updateTask).delete(checkAuth, deleteTask);
