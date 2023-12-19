f6f4a268eab863a05fd824f9856af9a9c48e00e8
gabicilfone <cilfonegabriel@gmail.com>
2023-12-19 10:42:25 -0300

    Add task to an project

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,6 +1,28 @@
-const addTask = async (req, res) => {
+import Project from "../models/Project.js";
+import Task from "../models/Task.js";
 
-}
+const addTask = async (req, res) => {
+    const { project } = req.body;
+
+    const projectExists = await Project.findById(project);
+
+    if (!projectExists) {
+        const error = new Error("The Project does not exist");
+        return res.status(404).json({ msg: error.message });
+    }
+
+    if(projectExists.creator.toString() !== req.user._id.toString()){
+        const error = new Error("You do not have permissions to add tasks.");
+        return res.status(404).json({ msg: error.message });
+    }
+
+    try {
+        const storeTask = await Task.create(req.body);
+        res.json(storeTask);
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 const getTask = async (req, res) => {
     
