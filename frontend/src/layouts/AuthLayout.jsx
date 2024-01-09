17400a4cc21725c2f63ff24a0f86c47a1ba3e360
gabicilfone <cilfonegabriel@gmail.com>
2024-01-09 11:43:57 -0300

    Add principal Layout

--- frontend/src/layouts/AuthLayout.jsx
+++ frontend/src/layouts/AuthLayout.jsx
@@ -2,11 +2,12 @@ import { Outlet } from "react-router-dom"
 const AuthLayout = () => {
   return (
     <>
-        <div>
-            AuthLayout
-        </div>
+        <main className="container mx-auto mt-5 md:mt-20 p-5 md:flex md:justify-center">
+            <div className="md:w-2/3 lg:h-1/2">
+                <Outlet />
+            </div>
+        </main>
 
-        <Outlet />
     </>
 
   )
