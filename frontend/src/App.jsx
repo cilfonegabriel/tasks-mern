d474acd14738fb8e580fd188c145e5d168355c64
gabicilfone <cilfonegabriel@gmail.com>
2024-01-09 11:43:22 -0300

    add pagges to app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthLayout from "./layouts/AuthLayout";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import ForgetPassword from "./pages/ForgetPassword";
+import NewPassword from "./pages/NewPassword";
+import ConfirmAccount from "./pages/ConfirmAccount";
 
 function App() {
 
@@ -11,7 +14,9 @@ function App() {
         <Route path="/" element={<AuthLayout />}>
           <Route index element={<Login />} />
           <Route path="register" element={<Register />} />
-
+          <Route path="forget-password" element={<ForgetPassword />} />
+          <Route path="forget-password/:token" element={<NewPassword />} />
+          <Route path="confirm/:id" element={<ConfirmAccount />} />
         </Route>
       </Routes>
     </BrowserRouter>
