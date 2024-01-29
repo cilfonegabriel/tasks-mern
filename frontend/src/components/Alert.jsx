5dec7e6053bf1f19ecda7027a878bca293c07613
gabicilfone <cilfonegabriel@gmail.com>
2024-01-29 11:08:03 -0300

    Add message of error

--- frontend/src/components/Alert.jsx
+++ frontend/src/components/Alert.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
 
-const Alert = () => {
+const Alert = ({alert}) => {
   return (
-    <div>
-      
+    <div className={`${alert.errot ? 'from-red-400 to-red-600' : 'from-sky-400 to-sky-600'} bg-gradient-to-br text-center p-3 rounded-xl uppercase
+    text-white font-bold text-sm my-10 `}>
+      {alert.msg}
     </div>
   )
 }
