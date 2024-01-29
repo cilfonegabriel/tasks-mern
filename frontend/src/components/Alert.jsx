63d645ed5ab8f364360eb80fb85e26ac7e52b4ba
gabicilfone <cilfonegabriel@gmail.com>
2024-01-29 11:13:33 -0300

    Add style to the message of alert

--- frontend/src/components/Alert.jsx
+++ frontend/src/components/Alert.jsx
@@ -1,7 +1,7 @@
 
 const Alert = ({alert}) => {
   return (
-    <div className={`${alert.errot ? 'from-red-400 to-red-600' : 'from-sky-400 to-sky-600'} bg-gradient-to-br text-center p-3 rounded-xl uppercase
+    <div className={`${alert.error ? 'from-red-400 to-red-600' : 'from-sky-400 to-sky-600'} bg-gradient-to-br text-center p-3 rounded-xl uppercase
     text-white font-bold text-sm my-10 `}>
       {alert.msg}
     </div>
