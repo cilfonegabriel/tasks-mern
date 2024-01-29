cbec1958576abf3c3412037df56175c5a78dac31
gabicilfone <cilfonegabriel@gmail.com>
2024-01-29 10:58:45 -0300

    Add state to the form

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,84 +1,99 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 
 const Register = () => {
-  return (
-    <>
-      <h1 className="text-sky-600 font-black text-6xl capitalize">Create your account and manage your {""}
-          <span className="text-slate-700">projects</span>
-      </h1>
 
-          <form className="my-10 bg-white shadow rounded-lg p-10">
-              <div className="my-5">
-                  <label 
-                      className="uppercase text-gray-600 block text-xl font-bold"
-                      htmlFor="name"
-                  >Name</label>
-                  <input 
-                      id="name"
-                      type="text" 
-                      placeholder="Your Name"
-                      className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
-                  />
-              </div>
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [repeatPassword, setRepeatPassword] = useState('')
 
-              <div className="my-5">
-                  <label 
-                      className="uppercase text-gray-600 block text-xl font-bold"
-                      htmlFor="email"
-                  >Email</label>
-                  <input 
-                      id="email"
-                      type="email" 
-                      placeholder="Registration Email"
-                      className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
-                  />
-              </div>
-              <div className="my-5">
-                  <label 
-                      className="uppercase text-gray-600 block text-xl font-bold"
-                      htmlFor="password"
-                  >Password</label>
-                  <input 
-                      id="password"
-                      type="password" 
-                      placeholder="Registration Password"
-                      className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
-                  />
-              </div>
+    return (
+        <>
+        <h1 className="text-sky-600 font-black text-6xl capitalize">Create your account and manage your {""}
+            <span className="text-slate-700">projects</span>
+        </h1>
 
-              <div className="my-5">
-                  <label 
-                      className="uppercase text-gray-600 block text-xl font-bold"
-                      htmlFor="password2"
-                  >Repeat Password</label>
-                  <input 
-                      id="password2"
-                      type="password" 
-                      placeholder="Repeat your Password"
-                      className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
-                  />
-              </div>
+            <form className="my-10 bg-white shadow rounded-lg p-10">
+                <div className="my-5">
+                    <label 
+                        className="uppercase text-gray-600 block text-xl font-bold"
+                        htmlFor="name"
+                    >Name</label>
+                    <input 
+                        id="name"
+                        type="text" 
+                        placeholder="Your Name"
+                        className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                    />
+                </div>
 
-              <input 
-                  type="submit"
-                  value="Create Account"
-                  className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
-              />
-          </form>
+                <div className="my-5">
+                    <label 
+                        className="uppercase text-gray-600 block text-xl font-bold"
+                        htmlFor="email"
+                    >Email</label>
+                    <input 
+                        id="email"
+                        type="email" 
+                        placeholder="Registration Email"
+                        className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                </div>
+                <div className="my-5">
+                    <label 
+                        className="uppercase text-gray-600 block text-xl font-bold"
+                        htmlFor="password"
+                    >Password</label>
+                    <input 
+                        id="password"
+                        type="password" 
+                        placeholder="Registration Password"
+                        className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+                </div>
 
-          <nav className="lg:flex lg:justify-between">
-              <Link
-                  className="block text-center my-5 text-slate-500 uppercase text-sm"
-                  to="/"
-              >Do you already have an account? Log in</Link>
+                <div className="my-5">
+                    <label 
+                        className="uppercase text-gray-600 block text-xl font-bold"
+                        htmlFor="password2"
+                    >Repeat Password</label>
+                    <input 
+                        id="password2"
+                        type="password" 
+                        placeholder="Repeat your Password"
+                        className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+                        value={repeatPassword}
+                        onChange={e => setRepeatPassword(e.target.value)}
+                    />
+                </div>
 
-              <Link
-                  className="block text-center my-5 text-slate-500 uppercase text-sm"
-                  to="/forget-password"
-              >I forgot my password</Link>
-          </nav>
-        </>
-  )
+                <input 
+                    type="submit"
+                    value="Create Account"
+                    className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
+                />
+            </form>
+
+            <nav className="lg:flex lg:justify-between">
+                <Link
+                    className="block text-center my-5 text-slate-500 uppercase text-sm"
+                    to="/"
+                >Do you already have an account? Log in</Link>
+
+                <Link
+                    className="block text-center my-5 text-slate-500 uppercase text-sm"
+                    to="/forget-password"
+                >I forgot my password</Link>
+            </nav>
+            </>
+    )
 }
 
 export default Register
