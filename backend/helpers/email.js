04c780249b44fba0589fd17f49b1544f83332483
gabicilfone <cilfonegabriel@gmail.com>
2024-01-30 10:12:16 -0300

    Set up the email to welcome the user

--- backend/helpers/email.js
+++ backend/helpers/email.js
@@ -14,6 +14,15 @@ export const emailRegister = async (datos) => {
 
     //Information of the email
     const info = await transport.sendMail({
+        from: '"Tasks -Project Manager" <cuentas@tasks.com>',
+        to: email,
+        subject: 'Tasks - Check your account',
+        text: 'Check your account in Tasks',
+        html: `<p>Hi: ${name} Check your account in Tasks</p>
+        <p>Your account is almost ready, you just have to confirm in the following link:
+        <a href="${process.env.FRONTEND_URL}/confirm/${token}">Check Account</a>
+        <p>If you did not create this account you can ignore this message</p>
         
+        `,
     })
 }
