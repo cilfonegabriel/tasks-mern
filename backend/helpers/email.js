f48ab9a2497ce7192c2cb6142d11340c4bfee3eb
gabicilfone <cilfonegabriel@gmail.com>
2024-01-30 09:58:37 -0300

    Add mailtrap information

--- backend/helpers/email.js
+++ backend/helpers/email.js
@@ -1,5 +1,19 @@
 import nodemailer from 'nodemailer';
 
-export const emailRegister = (datos) => {
-    console.log("DATOS", datos)
+export const emailRegister = async (datos) => {
+    const { email, name, token } = datos;
+
+    const transport = nodemailer.createTransport({
+        host: "sandbox.smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+          user: "5376fb5db071c5",
+          pass: "50815230964e2b"
+        }
+    });
+
+    //Information of the email
+    const info = await transport.sendMail({
+        
+    })
 }
