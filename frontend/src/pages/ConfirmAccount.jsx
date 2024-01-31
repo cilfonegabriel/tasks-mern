cf6a098b71e1ccd4193b7378e1fdb7429abc934f
gabicilfone <cilfonegabriel@gmail.com>
2024-01-31 09:56:19 -0300

    Add variable with route

--- frontend/src/pages/ConfirmAccount.jsx
+++ frontend/src/pages/ConfirmAccount.jsx
@@ -4,6 +4,22 @@ import axios from "axios"
 import Alert from "../components/Alert"
 
 const ConfirmAccount = () => {
+
+  const params = useParams();
+
+  const { id } = params
+  
+  useEffect(() => {
+    const confirmAccount = async () => {
+      try {
+        const url = `http://localhost:4000/api/users/confirm/${id}`
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    confirmAccount();
+  }, [])
+
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">Confirm your account and start creating your {""}
