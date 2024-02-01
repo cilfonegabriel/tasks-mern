671c4eb7ebcd04bf44a3c21614ad1c78e9d3cfa9
gabicilfone <cilfonegabriel@gmail.com>
2024-02-01 16:08:36 -0300

    Add state

--- frontend/src/pages/ForgetPassword.jsx
+++ frontend/src/pages/ForgetPassword.jsx
@@ -1,13 +1,20 @@
 import { Link } from "react-router-dom"
+import { useState } from "react"
 
 const ForgetPassword = () => {
+
+    const [email, setEmail] = useState('')
+
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">Recover your access and don't lose your {""}
           <span className="text-slate-700">projects</span>
       </h1>
 
-          <form className="my-10 bg-white shadow rounded-lg p-10">
+          <form 
+            className="my-10 bg-white shadow rounded-lg p-10"
+            onSubmit={handleSubmit}    
+        >
 
               <div className="my-5">
                   <label 
@@ -19,6 +26,8 @@ const ForgetPassword = () => {
                       type="email" 
                       placeholder="Registration Email"
                       className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+                      value={email}
+                      onChange={e => setEmail(e.target.value)}
                   />
               </div>
               <input 
