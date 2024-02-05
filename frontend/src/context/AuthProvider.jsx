f1456ec58a3aacdb51e6d40fe87b35269e3fd14f
gabicilfone <cilfonegabriel@gmail.com>
2024-02-05 14:12:54 -0300

    Checking the token to authenticate the user

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, createContext } from "react";
+import customerAxios from "../config/customerAxios";
 
 const AuthContext = createContext();
 
@@ -6,8 +7,31 @@ const AuthProvider = ({children}) => {
 
     const [auth, setAuth] = useState({})
 
+    useEffect(() => {
+        const authenticateUser = async () => {
+            const token = localStorage.getItem('token');
 
-    
+            if(!token) {
+                return
+            }
+
+            const config = {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization:`Bearer ${token}`
+                }
+            }
+
+            try {
+                const { data } = await customerAxios('/users/profile', config)
+                setAuth(data)
+            } catch (error) {
+                
+            }
+        }
+        authenticateUser();
+    }, []);
+
     return (
         <AuthContext.Provider
             value={{
