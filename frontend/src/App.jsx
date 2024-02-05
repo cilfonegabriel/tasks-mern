c6d6b1193f69359c82323f4c7702179d8251811e
gabicilfone <cilfonegabriel@gmail.com>
2024-02-05 13:49:09 -0300

    Add AuthProvider in the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,21 +5,23 @@ import Register from "./pages/Register";
 import ForgetPassword from "./pages/ForgetPassword";
 import NewPassword from "./pages/NewPassword";
 import ConfirmAccount from "./pages/ConfirmAccount";
-
+import { AuthProvider } from "./context/AuthProvider";
 
 function App() {
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AuthLayout />}>
-          <Route index element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="forget-password" element={<ForgetPassword />} />
-          <Route path="forget-password/:token" element={<NewPassword />} />
-          <Route path="confirm/:id" element={<ConfirmAccount />} />
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<AuthLayout />}>
+            <Route index element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path="forget-password" element={<ForgetPassword />} />
+            <Route path="forget-password/:token" element={<NewPassword />} />
+            <Route path="confirm/:id" element={<ConfirmAccount />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   )
 }
