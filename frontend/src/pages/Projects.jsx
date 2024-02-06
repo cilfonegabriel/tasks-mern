514146a9572d3a05a6244bf155e235a45ea501e6
gabicilfone <cilfonegabriel@gmail.com>
2024-02-06 11:07:55 -0300

    Cretaing a context of prjects

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import useProjects from '../hooks/useProjects'
 
 const Projects = () => {
+
+  const {projects} = useProjects()
+  
   return (
     <>
       <h1 className='text-4xl font-black'>Projects</h1>
       <div>
-        
+
       </div>
     </>
   )
