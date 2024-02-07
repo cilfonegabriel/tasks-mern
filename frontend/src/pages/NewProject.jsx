1cb223cf0aa3ff9c7fe04952d8fc4bbd9f1d6cf3
gabicilfone <cilfonegabriel@gmail.com>
2024-02-07 15:13:51 -0300

    Import component form project to thepage new project

--- frontend/src/pages/NewProject.jsx
+++ frontend/src/pages/NewProject.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import FormProject from "../components/FormProject"
 
 const NewProject = () => {
   return (
     <>
-      <h1 className='text-4xl font-black'>NewProject</h1>
-      <div>
-        
+      <h1 className='text-4xl font-black'>Create Project</h1>
+      <div className='mt-10 flex justify-center'>
+        <FormProject/>
       </div>
     </>
   )
