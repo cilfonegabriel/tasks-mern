093a7ede15195a6d633f74f390307e5fb5d5a4ba
gabicilfone <cilfonegabriel@gmail.com>
2024-02-07 15:20:59 -0300

    Add descdescription to the form

--- frontend/src/components/FormProject.jsx
+++ frontend/src/components/FormProject.jsx
@@ -9,24 +9,41 @@ const FormProject = () => {
 
     return (
         <form className='bg-white py-10 px-5 md:w-1/2 rounded-lg shadow'>
-        <div>
-            <label
-                className='text-gray-700 uppercase font-bold text-sm'
-                htmlFor='name'
-            >
-                Name Project
-            </label>
+            <div className="mb-5">
+                <label
+                    className='text-gray-700 uppercase font-bold text-sm'
+                    htmlFor='name'
+                >
+                    Name Project
+                </label>
 
-            <input
-            id='name'
-                type='text'
-                className='border w-full p-2 mt-2 placeholder-gray-400 rounded-md'
-                placeholder='Name of the Project'
-                value={name}
-                onChange={e => setName(e.target.value)}
-            />
-            
-        </div>
+                <input
+                    id='name'
+                    type='text'
+                    className='border w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+                    placeholder='Name of the Project'
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                />
+            </div>
+
+            <div className="mb-5">
+                <label
+                    className='text-gray-700 uppercase font-bold text-sm'
+                    htmlFor='description'
+                >
+                    Description
+                </label>
+
+                <textarea
+                    id='description'
+                    className='border w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+                    placeholder='Description of the Project'
+                    value={description}
+                    onChange={e => setDescription(e.target.value)}
+                />
+                
+            </div>
         </form>
     )
 }
