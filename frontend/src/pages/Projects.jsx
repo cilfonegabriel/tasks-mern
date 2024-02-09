efd12d24740b28be01687a12c992a79f55d9e6e3
gabicilfone <cilfonegabriel@gmail.com>
2024-02-09 16:25:09 -0300

    Use useEffect for get projects

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -4,12 +4,13 @@ import useProjects from '../hooks/useProjects'
 const Projects = () => {
 
   const {projects} = useProjects()
+  console.log(projects)
   
   return (
     <>
       <h1 className='text-4xl font-black'>Projects</h1>
       <div>
-
+        
       </div>
     </>
   )
