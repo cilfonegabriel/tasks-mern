637b726a5278f76a85da24791a3bdf1db305ce64
gabicilfone <cilfonegabriel@gmail.com>
2024-02-09 16:29:26 -0300

    Show message ig there is project

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -9,8 +9,8 @@ const Projects = () => {
   return (
     <>
       <h1 className='text-4xl font-black'>Projects</h1>
-      <div>
-        
+      <div className='bg-white shadow mt-10 rounded-lg'>
+        {projects.length ? <p>Si hay proyectos</p> : <p className=' text-center text-gray-600 uppercase p-5'>No hay proyect</p>}
       </div>
     </>
   )
