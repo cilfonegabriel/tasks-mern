d955a68edf2230da50b27e59d69465fc6209a937
gabicilfone <cilfonegabriel@gmail.com>
2024-02-15 16:01:47 -0300

    Add icon to button for add tasks

--- frontend/src/pages/Project.jsx
+++ frontend/src/pages/Project.jsx
@@ -32,8 +32,13 @@ const Project = () => {
 
             <button 
                 type="button"
-                className="font-bold uppercase text-sm px-5 py-3 w-full md:w-auto rounded-lg bg-sky-400 text-white text-center"
-            >New task</button>     
+                className="font-bold uppercase text-sm px-5 py-3 w-full md:w-auto rounded-lg bg-sky-400 text-white text-center mt-5 flex gap-2 items-center justify-center"
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                </svg>
+
+            New task</button>     
         </>
            
     )
