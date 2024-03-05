25515d126b8363e77503dc57b649fcb839c80c1e
gabicilfone <cilfonegabriel@gmail.com>
2024-03-05 17:39:23 -0300

    Add handle modal for delete a task

--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -35,7 +35,7 @@ const Task = ({task}) => {
 
         <button 
           className="bg-red-600 px-4 py-3 text-white uppercase font-bold text-sm rounded-lg"
-          onClick={handleModalDeleteTask}
+          onClick={() => handleModalDeleteTask(task)}
         >
           Delete
         </button>
