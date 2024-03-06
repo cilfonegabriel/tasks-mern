489966aa0718f56d444a47d4a1c35b63c530f7a3
gabicilfone <cilfonegabriel@gmail.com>
2024-03-06 15:05:22 -0300

    Add name of the project in the title

--- frontend/src/pages/NewCollaborator.jsx
+++ frontend/src/pages/NewCollaborator.jsx
@@ -1,9 +1,19 @@
 import FormCollaborator from "../components/FormCollaborator"
+import { useEffect } from "react"
+import useProjects from "../hooks/useProjects"
+import { useParams } from "react-router-dom"
 
 const NewCollaborator = () => {
+
+    const { getProject, project } = useProjects()
+    const params = useParams()
+
+    useEffect(() => {
+        getProject(params.id)
+    })
   return (
     <>
-      <h1 className="text-4xl font-black">Add Collaborator</h1>
+      <h1 className="text-4xl font-black">Add Collaborator to the project {project.name}</h1>
 
       <div className="mt-10 flex justify-center">
         <FormCollaborator />
