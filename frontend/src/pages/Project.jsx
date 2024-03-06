140c77b12dca41d069247a7a7a7fe6e4b230cfab
gabicilfone <cilfonegabriel@gmail.com>
2024-03-06 14:12:07 -0300

    Add collaborators option in the project page

--- frontend/src/pages/Project.jsx
+++ frontend/src/pages/Project.jsx
@@ -67,6 +67,17 @@ const Project = () => {
                 <p className="text-center my-5 p-10">There arent tasks in this Project</p>}
             </div>
 
+            <div className="flex items-center justify-between mt-10">
+                <p className="font-bold text-xl">Collaborators</p>
+                <Link
+                    to={`/projects/new-collaborator/${project._id}`}
+                    className="text-gray-400 hover:text-black uppercase font-bold"
+                >Add</Link>
+            </div>
+
+            
+
+
             <ModalFormTask />
             <ModalDeleteTask />
         </>
