2029dc4b8d3ea06b133e273539ce184cf42ad7b3
gabicilfone <cilfonegabriel@gmail.com>
2024-03-07 15:29:02 -0300

    Imports and show information to add collaborators

--- frontend/src/pages/NewCollaborator.jsx
+++ frontend/src/pages/NewCollaborator.jsx
@@ -5,12 +5,14 @@ import { useParams } from "react-router-dom"
 
 const NewCollaborator = () => {
 
-    const { getProject, project } = useProjects()
+    const { getProject, project, collaborator, loading, addCollaborator } = useProjects()
     const params = useParams()
 
     useEffect(() => {
         getProject(params.id)
-    })
+    },[])
+
+    if (loading) return loading
   return (
     <>
       <h1 className="text-4xl font-black">Add Collaborator to the project {project.name}</h1>
@@ -18,6 +20,27 @@ const NewCollaborator = () => {
       <div className="mt-10 flex justify-center">
         <FormCollaborator />
       </div>
+
+      {loading ? <p className= "text-center">loading...</p> : collaborator?._id &&(
+        <div className="flex justify-center mt-10">
+          <div className="bg-white py-10 px-5 md:w-1/2 rounded-lg shadow">
+            <h2 className="text-center mb-10 text-2xl font-bold">Result: </h2>
+
+            <div className="flex justify-between items-center">
+              <p>{collaborator.name}</p>
+
+              <button
+                type="button"
+                className="bg-slate-500 px-5 py-2 rounded-lg uppercase text-white font-bold text-sm"
+                onClick={() => addCollaborator({
+                  email: collaborator.email
+                })}
+              >Add to the project</button>
+            </div>
+
+          </div>
+        </div>
+      )}
     </>
   )
 }
